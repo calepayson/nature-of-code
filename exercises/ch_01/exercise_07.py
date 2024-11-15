e73d52f67c6074114eeaf6663fe26009f2513752
Cale Smith <calepayson@mac.com>
2024-11-15 10:16:37 -0800

    Complete exercise_07.py
    - Translate a few pseudocode statements to code.

---
+++ exercises/ch_01/exercise_07.py
@@ -0,0 +1,14 @@
+# Translate the following jpseudocode to code, using static or nonstatic 
+# function where appropriate:
+#
+# - The vector `v` equal (1, 5).
+# - The vector `u` equals `v` multiplied by 2.
+# - The vector `w` equals `v` minus `u`.
+# - Divide the vector `w` by 3
+
+from p5 import *
+
+v = Vector(1, 5)
+u = v * 2
+w = v - u
+v / 3
