977ed72d96764bcbad6c8c3aa8bae8928c67d7b3
Cale Smith <calepayson@mac.com>
2024-11-21 09:02:24 -0800

    Add 016-rotating_baton.py
    - Draws a baton that rotates around a central axis.

---
+++ one_offs/016-rotating_baton.py
@@ -0,0 +1,32 @@
+from p5 import *
+
+# Global angle variables
+angle = 0
+angle_velocity = 0
+angle_acceleration = 0.0001
+
+def draw():
+    """Draw a single frame on the p5 canvas."""
+    global angle, angle_velocity, angle_acceleration
+
+    # Erase the canvas
+    background(255) # White
+
+    # Update the coordinate plane
+    translate(width / 2, height / 2)
+    rotate(angle)
+
+    # Draw the baton
+    stroke(0)   # Black
+    fill(127)   # Gray
+    line(-60, 0, 60, 0)
+    circle(60, 0, 16)
+    circle(-60, 0, 16)
+
+    # Update the angle
+    angle_velocity += angle_acceleration
+    angle += angle_velocity
+
+
+# Run the p5 program
+run()
