3b9f70a4dbec7fc77b6aebba3fc552f71efe3795
Cale Smith <calepayson@mac.com>
2024-11-24 16:33:41 -0800

    Update workbook.py while playing with oscillation.
    - Visualizes a sin wave by oscillating a ball on the screen.

--- exercises/ch_03/workbook.py
+++ exercises/ch_03/workbook.py
@@ -1,37 +1,40 @@
 from p5 import *
-from objects import Mover
-import random
 
 
-# Adjust the mass of the mouse
-MOUSE_MASS = 5
+# Adjust the amplitude and period
+AMPLITUDE = width / 3
+PERIOD = 120
 
-# Adjust the min and max gravitational force (for asthetic reasons).
-MIN_GRAVITY = 2
-MAX_GRAVITY = 25
-
-# Adjust how much velocity is retained upon contact with the wall
-BOUNCE_COEFFICIENT = 0.9
-
-position = Vector(width / 2, height / 2)
-mass = 2.5
-mover = Mover(position, mass)
+# Initialize a global variable to keep track of time
+tx = 0.0
 
 def draw():
-    global mover
+    """
+    Draw a single frame on the p5 canvas.
+    """
+    # Pull in the global time variable
+    global tx
 
-    background(255)
+    # Reset the canvas
+    background(255) # White
 
-    mouse = Vector(mouse_x, mouse_y)
-    direction = mouse - mover.position
-    magnitude = (MOUSE_MASS * mover.mass) / (direction.mag() ** 2)
-    direction.normalize()
-    gravity = constrain(magnitude, MIN_GRAVITY, MAX_GRAVITY) * direction
-    
-    mover.apply_force(gravity)
-    
-    mover.update()
-    mover.check_edges(BOUNCE_COEFFICIENT)
-    mover.show()
+    # Calculate the x value of the ball
+    x = sin((tx / PERIOD) * 2 * PI) * AMPLITUDE
 
+    # Reset the pallet
+    stroke(0)
+    fill(127)
+
+    # Move the origin to the center of the screen
+    translate(width / 2, height / 2)
+
+    # Draw the objects
+    line(0, 0, x, 0)
+    circle(x, 0, 48)
+
+    # Increment time
+    tx += 1
+
+
+# Run the p5 program 
 run()
