38eca6810346d79f5dae2e3948a867179e545e87
Cale Smith <calepayson@mac.com>
2024-11-27 10:33:11 -0800

    Update workbook.py to show basic spring physics
    - Use Hooke's law to calculate the force of a spring.

--- exercises/ch_03/workbook.py
+++ exercises/ch_03/workbook.py
@@ -1,95 +1,56 @@
 from p5 import *
-import random
 
-# Adjust distance between wave nodes
-STEP = 20
-
-# Adjust the speed at which the wave moves
-SPEED = 10
-
-# Adjust the number of waves
-NUMBER_OF_WAVES = 3
-
-# Adjust min and max period
-MIN_PERIOD = 300
-MAX_PERIOD = 750
-
-# Adjust min and max amplitude
-MIN_AMPLITUDE = 30
-MAX_AMPLITUDE = 150
-
-class Wave:
-    """
-    A generic representation of a wave.
-
-    Attributes:
-        period (int): The period of the wave in pixels.
-        amplitude (int): The amplitude of the wave in pixels.
-    """
-    def __init__(self, period: int, amplitude: int):
-        """
-        Construct a new Wave object.
-
-        Args:
-            period (int): The period of the wave in pixels.
-            amplitude (int): The amplitude of the wave in pixels.
-        """
-        self.period = period
-        self.amplitude = amplitude
-
-    def get_height(self, x):
-        """
-        Get the height of the wave at a given x position.
-
-        Args:
-            x (float): The x coordinate of the desired point.
-
-        Returns: 
-            float: The y coordinate of the desired point.
-        """
-        return sin(TWO_PI * x / self.period) * self.amplitude
-
-# Initialize global variables
-start = 0
-waves = []
+# Hooke's Law:
+# F = -k * x
+# Where:
+#   - F is the spring force.
+#   - k is the spring constant.
+#   - x is the distance from rest.
 
 
-# Initalize all wave objects
-for _ in range(NUMBER_OF_WAVES):
-    period = random.randint(MIN_PERIOD, MAX_PERIOD)
-    amplitude = random.randint(MIN_AMPLITUDE, MAX_AMPLITUDE)
-    wave = Wave(period, amplitude)
-    waves.append(wave)
+
+class Bob:
+    def __init__(self):
+        self.anchor = Vector(width / 2, 0)
+        self.position = Vector(width / 2, 120)
+        self.velocity = Vector(0, 0)
+        self.acceleration = Vector(0, 0)
+
+    def apply_force(self, force: Vector):
+        self.acceleration += force
+
+    def update(self):
+        self.velocity += self.acceleration
+        self.position += self.velocity
+
+        self.acceleration *= 0
+
+    def show(self):
+        stroke(0)
+        fill(127)
+        line(self.anchor.x, self.anchor.y, self.position.x, self.position.y)
+        circle(self.position.x, self.position.y, 48)
+
+
+bob = Bob()
 
 def draw():
-    """
-    Draw a single frame on the p5 canvas.
-    """
-    # Pull in the global variables (necessary for p5)
-    global waves, start
+    global bob
 
-    # Reset the canvas
-    background(255) # White
+    background(255)
 
-    # For every circle...
-    for x in range(0, width, STEP):
-        # Set the origin of y to the center of the screen
-        y = height / 2
+    k = 0.1
+    rest_length = 100
 
-        # Add up the amplitudes of each wave at x
-        for wave in waves:
-            y += wave.get_height(x + start)
+    direction = bob.position - bob.anchor
+    current_length = direction.mag()
+    direction.normalize()
+    x = float(current_length - rest_length)
+    force = -1 * k * x * direction
 
-        # Reset the pallete
-        stroke(0)       # Black
-        fill(127, 127)  # Gray, 50% opacity
+    bob.apply_force(force)
 
-        # Draw the circle
-        circle(x, y, 48)
+    bob.update()
+    bob.show()
 
-    # Increment the starting position
-    start += SPEED
-
-
-# Run the p5 program
 run()
